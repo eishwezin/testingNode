9907156fb0c41b70554be49b16cf0b6bb8b2fa6a
eishwezin <thandar789820@gmail.com>
2019-04-24 21:25:13 +0630

    add project

---
+++ Model/User.js
@@ -0,0 +1,39 @@
+var mongoose=require ('mongoose');
+var Schema=mongoose.Schema;
+
+
+var UserSchema= new Schema({
+  firstname:{
+    type:String,
+    required:true,
+
+  },
+  lastname:{
+    type:String,
+    required:true,
+
+  },
+  email:{
+    type:String,
+    required:true,
+    unique:true
+  },
+  zipcode:{
+    type:String,
+    required:true
+
+  },
+  city:{
+    type:String,
+    required:true
+
+  },
+  state:{
+    type:String,
+    required:true
+
+  }
+
+
+})
+module.exports=mongoose.model('User',UserSchema)
